c300b95683b3a50ed2b4737b91e23c73bba37f3f
Frans Narciso <fransnarc@gmail.com>
2024-11-05 13:44:33 -0600

    Added 5 customers. Added validations to customer table.

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Customer do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :full_name, :phone_number, :email_address, :notes
+  permit_params :full_name, :phone_number, :email_address, :notes, :image
   #
   # or
   #
@@ -14,5 +14,15 @@ ActiveAdmin.register Customer do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
+
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions
+  end
   
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,14 @@
 class Customer < ApplicationRecord
+    validates :full_name, :phone_number, :email_address, presence: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+
     has_one_attached :image
+
+    def self.ransackable_associations(auth_object = nil)
+        ["image_attachment", "image_blob"]
+      end
+
+    def self.ransackable_attributes(auth_object = nil)
+        ["created_at", "email_address", "full_name", "id", "notes", "phone_number", "updated_at"]
+    end
 end
